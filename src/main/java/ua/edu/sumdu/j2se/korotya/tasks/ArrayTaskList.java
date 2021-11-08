b19e764e64180becff9a37fd18798ecfb8a5809b
korotyaNikita <korotinnikit@gmail.com>
2021-11-08 14:57:02 +0200

    Practice 3. Add new ArrayTaskList class

---
+++ src/main/java/ua/edu/sumdu/j2se/korotya/tasks/ArrayTaskList.java
@@ -0,0 +1,56 @@
+package ua.edu.sumdu.j2se.korotya.tasks;
+
+public class ArrayTaskList {
+    private Task[] tasks = new Task[10];
+    private int size;
+
+    public void add(Task task) {
+        if (size >= tasks.length) {
+            Task[] tasksCopy = tasks;
+            tasks = new Task[tasks.length * 2];
+
+            System.arraycopy(tasksCopy, 0, tasks, 0, tasksCopy.length);
+        }
+
+        tasks[size] = task;
+        size++;
+    }
+
+    public boolean remove(Task task) {
+        int index = -1;
+        for (int i = 0; i < size; i++)
+            if (tasks[i] == task) {
+                index = i;
+                break;
+            }
+
+        if (index == -1)
+            return false;
+
+        tasks[index] = null;
+        for (int i = index + 1; i < size; i++) {
+            tasks[i - 1] = tasks[i];
+            tasks[i] = null;
+        }
+        size--;
+
+        return true;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Task getTask(int index) {
+        return index >= size ? null : tasks[index];
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
+        ArrayTaskList arrayTaskList = new ArrayTaskList();
+        for (int i = 0; i < size; i++)
+            if (tasks[i].nextTimeAfter(from) != -1 && tasks[i].nextTimeAfter(to) == -1)
+                arrayTaskList.add(tasks[i]);
+
+        return arrayTaskList;
+    }
+}
